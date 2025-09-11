31bdd00318c359a195690377f2e478a76b7f2ddf
wahyukrisnaaji32 <wahyukrisnaaji32@gmail.com>
2025-09-11 22:06:00 +0700

    fix(generate-grpc.psl): update generate *.proto

--- generate-grpc.ps1
+++ generate-grpc.ps1
@@ -4,6 +4,6 @@ protoc `
   --go-grpc_out=. `
   --go_opt=paths=source_relative `
   --go-grpc_opt=paths=source_relative `
-  internal/grpc/wallet.proto
+  internal/grpc/*.proto
 
 Write-Host "✅ gRPC code generated successfully!" -ForegroundColor Green
